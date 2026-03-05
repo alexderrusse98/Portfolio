8a2db7e7d88643756b44184949942071eeffbd6f
alexderrusse98 <alex.weiswasser.com@gmail.com>
2026-03-05 19:35:43 +0100

    skill_section

--- src/app/pages/home/skills/skills.component.ts
+++ src/app/pages/home/skills/skills.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  showLine = false;
 
+skills = [
+  { id: 'html', label: 'HTML' },
+  { id: 'css', label: 'CSS' },
+  { id: 'js', label: 'JavaScript' },
+  { id: 'ts', label: 'TypeScript' },
+  { id: 'angular', label: 'Angular' },
+  { id: 'firebase', label: 'Firebase' },
+  { id: 'git', label: 'Git' },
+  { id: 'restApi', label: 'REST-API' },
+  { id: 'scrum', label: 'Scrum' },
+  { id: 'material', label: 'Material Design' },
+];
+
+  ngOnInit() {
+    setInterval(() => {
+      this.showLine = true;
+      setTimeout(() => this.showLine = false, 3000);
+    }, 5000);
+  }
 }
