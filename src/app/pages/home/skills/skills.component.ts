cce6505b834f8b5954e99ab040aeed758f91650a
alexderrusse98 <alex.weiswasser.com@gmail.com>
2026-03-06 13:49:25 +0100

    skills sticker content & open & close

--- src/app/pages/home/skills/skills.component.ts
+++ src/app/pages/home/skills/skills.component.ts
@@ -11,23 +11,35 @@ import { CommonModule } from '@angular/common';
 export class SkillsComponent {
   showLine = false;
 
-skills = [
-  { id: 'html', label: 'HTML' },
-  { id: 'css', label: 'CSS' },
-  { id: 'js', label: 'JavaScript' },
-  { id: 'ts', label: 'TypeScript' },
-  { id: 'angular', label: 'Angular' },
-  { id: 'firebase', label: 'Firebase' },
-  { id: 'git', label: 'Git' },
-  { id: 'restApi', label: 'REST-API' },
-  { id: 'scrum', label: 'Scrum' },
-  { id: 'material', label: 'Material Design' },
-];
+  skills = [
+    { id: 'html', label: 'HTML' },
+    { id: 'css', label: 'CSS' },
+    { id: 'js', label: 'JavaScript' },
+    { id: 'ts', label: 'TypeScript' },
+    { id: 'angular', label: 'Angular' },
+    { id: 'firebase', label: 'Firebase' },
+    { id: 'git', label: 'Git' },
+    { id: 'restApi', label: 'REST-API' },
+    { id: 'scrum', label: 'Scrum' },
+    { id: 'material', label: 'Material Design' },
+  ];
 
   ngOnInit() {
     setInterval(() => {
       this.showLine = true;
       setTimeout(() => this.showLine = false, 3000);
     }, 5000);
+  }
+
+  peelState: 'closed' | 'half' | 'open' = 'closed';
+
+  onPeel() {
+    if (this.peelState === 'closed') {
+      this.peelState = 'half';
+      setTimeout(() => this.peelState = 'open', 100); 
+    }
+    if (this.peelState === 'open') {
+      this.peelState = 'closed';
+    }
   }
 }
