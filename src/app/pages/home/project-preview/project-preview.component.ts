1bbfec61b111fbc5ff7cf8437e8a7f00e68b4a0a
alexderrusse98 <alex.weiswasser.com@gmail.com>
2026-03-07 15:08:55 +0100

    projects imgs

--- src/app/pages/home/project-preview/project-preview.component.ts
+++ src/app/pages/home/project-preview/project-preview.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-preview',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-preview.component.html',
   styleUrl: './project-preview.component.scss'
 })
 export class ProjectPreviewComponent {
+
+
 
 }
