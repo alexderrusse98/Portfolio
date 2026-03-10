4f1f53885a91ec5575a1c5b1df23dfab2e4c3266
alexderrusse98 <alex.weiswasser.com@gmail.com>
2026-03-10 13:31:00 +0100

    change img Background

--- src/app/pages/home/project-preview/project-preview.component.ts
+++ src/app/pages/home/project-preview/project-preview.component.ts
@@ -10,6 +10,28 @@ import { CommonModule } from '@angular/common';
 })
 export class ProjectPreviewComponent {
 
-
+  projects = [
+    {
+      id: 1,
+      title: 'Join',
+      description: 'Dies ist ein kurzer Beispieltext als Platzhalter für Inhalte. Er zeigt, wie ein Abschnitt mit mehreren Zeilen aussehen kann. Der Text hat keine echte Bedeutung und dient nur zum Layouten.',
+      imageUrl: 'assets/imgs/projects/join_notebook.png',
+      imgExtra: 'assets/imgs/projects/full_sticker.png',
+    },
+    {
+      id: 2,
+      title: 'El Pollo Loco',
+      description: 'Dies ist ein kurzer Beispieltext als Platzhalter für Inhalte. Er zeigt, wie ein Abschnitt mit mehreren Zeilen aussehen kann. Der Text hat keine echte Bedeutung und dient nur zum Layouten.',
+      imageUrl: 'assets/imgs/projects/El_Pollo_Locco.png',
+      imgExtra: '',
+    },
+    {
+      id: 3,
+      title: 'Pokedex',
+      description: 'Dies ist ein kurzer Beispieltext als Platzhalter für Inhalte. Er zeigt, wie ein Abschnitt mit mehreren Zeilen aussehen kann. Der Text hat keine echte Bedeutung und dient nur zum Layouten.',
+      imageUrl: 'assets/imgs/projects/pokedex.png',
+      imgExtra: '',
+    }
+  ]
 
 }
