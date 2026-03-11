498ec26b5310c89958a184f7e9300298dec2ff01
alexderrusse98 <alex.weiswasser.com@gmail.com>
2026-03-11 22:16:10 +0100

    contact-section add content

--- src/app/pages/home/contact/contact.component.ts
+++ src/app/pages/home/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AnimatedLineComponent } from '../../../shared/animated-line/animated-line.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [AnimatedLineComponent],
+  imports: [AnimatedLineComponent, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
